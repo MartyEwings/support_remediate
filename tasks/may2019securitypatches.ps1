4b776175b17f5854852db453ab3ae5faff4a3186
Jas <30461450+jasy125@users.noreply.github.com>
2019-07-05 15:08:52 +0100

    changed method of installs

--- tasks/may2019securitypatches.ps1
+++ tasks/may2019securitypatches.ps1
@@ -28,9 +28,67 @@
 #
 
 #https://www.powershellgallery.com/packages/PSWindowsUpdate/1.5.2.2/Content/Get-WUInstall.ps1
-#Requires powershell v3 but Compatible Version Windows 8, server 2012 and later as ps v3 is included by default
  
+try {
+    $psVersion = $PSVersionTable.PSVersion
+    } catch {
+      $psVersion = $null
+    }
+    
+    if(($psVersion -ne $null) -or ($psVersion.Major -gt 2)) { 
+    
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+    Install-Package -Name PSWindowsUpdate -Force
+    Import-Module PSWindowsUpdate
+    Get-WUServiceManager
 
-Install-Module PSWindowsUpdate -force
-$KBList = "KB4499179","KB4499154","KB4494440","KB4499171","KB4499167","KB4499181","KB4499165","KB4499151","KB4499158","KB4494441"
-Get-WUInstall -KBArticleID $KBList -AcceptAll
+        $KBList = "KB4499179","KB4499154","KB4494440","KB4499171","KB4499167","KB4499181","KB4499165","KB4499151","KB4499158","KB4494441"
+
+        Get-WUList | % {
+           if ($KBList.Contains($_.KB)) {
+             Get-WUInstall -KBArticleID $_.KB -AcceptAll -IgnoreReboot
+           } 
+        }
+    } else {
+
+        $Source="https://gallery.technet.microsoft.com/scriptcenter/2d191bcd-3308-4edd-9de2-88dff796b0bc/file/41459/43/PSWindowsUpdate.zip"
+        $Destination = "c:\temp\PSWindowsUpdate.zip"
+        $UnzipTo = "c:\temp"
+        $PSModulePath="$env:windir\System32\WindowsPowerShell\v1.0\Modules"
+        
+        If ( test-path -path "c:\temp"){}else{ mkdir "c:\temp" }
+        
+        Invoke-WebRequest $Source -OutFile $Destination -Verbose
+         
+        #Function to unzip the PSWindowsUpdate.zip 
+        function unzip($file, $destination)
+         {
+         $shell = new-object -com shell.application
+         $zip = $shell.NameSpace($file)
+         
+         foreach($item in $zip.items())
+            {
+                $shell.Namespace($destination).copyhere($item)
+            }
+         }
+         
+        #Unzip the PSWindowsUpdate.zip to CC:\temp to "$env:windir\System32\WindowsPowerShell\v1.0\Modules"
+        unzip -file $Destination -destination $UnzipTo 
+         
+        #Copy all the files from PSWindowsUpdate to 
+        if(Test-Path "$PSModulePath\PSWindowsUpdate"){}else{Copy-Item -Recurse $UnzipTo\PSWindowsUpdate -Destination $PSModulePath}
+        
+        #Import the PSWinodwsUpdate Module 
+        Import-Module PSWindowsUpdate
+         
+        #Get the Update server 
+        Get-WUServiceManager 
+        #Check, Download and Install Update 
+        #You will need to run this few time to ensure that there is no more update 
+        $KBList = "KB4499179","KB4499154","KB4494440","KB4499171","KB4499167","KB4499181","KB4499165","KB4499151","KB4499158","KB4494441"
+        Get-WUList | % {
+            if ($KBList.Contains($_.KB)) {
+              Get-WUInstall -KBArticleID $_.KB -AcceptAll -IgnoreReboot
+            } 
+         }
+    }
