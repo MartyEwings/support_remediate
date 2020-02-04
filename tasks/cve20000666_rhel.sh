5b76d1c231d6b1f65f65449f5ef33a20caeccb3d
martyewings <martyewings@gmail.com>
2020-02-04 15:38:48 +0000

    dbus demo task

--- tasks/cve20000666_rhel.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Puppet Task Name: cve20000666
-
-sudo yum update nfs-utils -y
-
---
+++ tasks/cve201912749_dbus.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Puppet Task Name: cve201912749_dbus
+#
+
+/usr/bin/yum update -y dbus*
